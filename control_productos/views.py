f336b1d0eb3435fd9213d6ce30be2a7090206072
Victorius23 <L20240002@smartin.tecnm.mx>
2024-04-02 14:32:29 -0600

    viewsets de control_productos

--- control_productos/views.py
+++ control_productos/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+import json
 # Create your views here.
+
+from .models import Producto, Categoria
+from .serializers import ProductoSerializer, CategoriaSerializer
+
+
+class ProductoViewApi(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+# * get categorias
+class CategoriaViewApi(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    
+# * recuperar los productos por categoria
+class ProductoCategoriaViewApi(viewsets.ModelViewSet):
+    serializer_class = ProductoSerializer
+
+    def get_queryset(self):
+        categoria = self.request.GET.get('categoria')
+        return Producto.objects.filter(categoria=categoria)
