b199a34bc38fcbd5bcc0d99af73c6ce3551a6a3c
Victorius23 <L20240002@smartin.tecnm.mx>
2024-04-02 14:36:49 -0600

    viewsets

--- control_ventas/views.py
+++ control_ventas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Venta
+from rest_framework import viewsets
+from .serializers import VentaSerializer
 
 # Create your views here.
+
+class VentaViewApi(viewsets.ModelViewSet):
+    queryset = Venta.objects.all()
+    serializer_class = VentaSerializer
