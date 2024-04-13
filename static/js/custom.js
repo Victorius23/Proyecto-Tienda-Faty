1b0fd6429fd85487f2e672e652daff18dbe40759
Victorius23 <L20240002@smartin.tecnm.mx>
2024-04-12 21:52:20 -0600

    Js de la contraseña y alertas en el login

---
+++ static/js/custom.js
@@ -0,0 +1,8 @@
+//se llama a la funcion de sweetalert los campos no pueden estar vacios
+Swal.fire({
+    title: 'Error!',
+    text: 'Los campos no pueden estar vacios',
+    icon: 'error',
+    confirmButtonText: 'Cerrar'
+    })
+
