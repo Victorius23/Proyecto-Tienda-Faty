00b44a7479d3428652ebe8f5aa5ea8b1db797587
Victorius23 <L20240002@smartin.tecnm.mx>
2024-04-12 21:51:57 -0600

    Cambio del control de inventario, de llave foranea a oneToOneField y creacion de cada producto en el inventario

--- control_inventario/models.py
+++ control_inventario/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -6,14 +7,28 @@ from control_productos.models import Producto
 
 
 class Inventario(models.Model):
-    Producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    producto = models.OneToOneField(Producto, on_delete=models.CASCADE, related_name='inventario')
     cantidad_stock = models.PositiveIntegerField()
     ultima_actualizacion = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.Producto} - {self.cantidad_stock}'
+        return f'{self.producto} - {self.cantidad_stock}'
     
     class Meta:
         verbose_name = 'Inventario'
         verbose_name_plural = 'Inventario'
+        
+        
+
+# * asegurarse de crear inventario para cada vez que se cree un producto
+
+@receiver(models.signals.post_save, sender=Producto)
+def crear_inventario(sender, instance, created, **kwargs):
+    if created:
+        Inventario.objects.create(producto=instance, cantidad_stock=0)
+        print(f'Inventario creado para {instance.nombre}')
+    else:
+        print(f'Inventario ya existe para {instance.nombre}')
+
+        
         
