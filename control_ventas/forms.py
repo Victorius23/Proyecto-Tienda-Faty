366cc2aa2da84b3e88a9f3aacb91a981aefb7de1
HetairoiElite <jonathan90090@gmail.com>
2024-05-03 17:44:14 -0600

    formulario personalizado detalle

--- control_ventas/forms.py
+++ control_ventas/forms.py
@@ -1,3 +1,29 @@
 from django import forms
 
 from .models import Venta, DetalleVenta
+
+
+# * detalle de venta
+class DetalleVentaForm(forms.ModelForm):
+    class Meta:
+        model = DetalleVenta
+        fields = ('producto', 'cantidad', 'importe')
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        producto = cleaned_data.get('producto')
+        cantidad = cleaned_data.get('cantidad')
+        
+        if cantidad > producto.inventario.cantidad_stock:
+            raise forms.ValidationError('No hay stock suficiente para el producto')
+        
+        return cleaned_data
+    
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.importe = instance.producto.precioventa * instance.cantidad
+        
+        if commit:
+            instance.save()
+            
+        return instance
