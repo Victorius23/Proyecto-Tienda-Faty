9b091afd2d172b78a86a754fac3d24c0662d12c1
HetairoiElite <jonathan90090@gmail.com>
2024-05-03 17:43:54 -0600

    guaradar el empleado que hizo la venta

--- control_ventas/admin.py
+++ control_ventas/admin.py
@@ -4,14 +4,8 @@ from django.contrib import admin
 
 from .models import Venta, DetalleVenta
 
-# importa el precio del modelo Producto
-from control_productos.models import Producto
 
-# importa librerias para transacciones y mensajes de error
-from django.db import transaction
-from django.core.exceptions import ValidationError
-from django.db import models
-from django import forms
+from .forms import DetalleVentaForm
 
 
 class DetalleVentaInline(admin.TabularInline):
@@ -21,6 +15,8 @@ class DetalleVentaInline(admin.TabularInline):
 
     # modelo detalle de venta y precio del producto
     model = DetalleVenta
+    
+    
     # se muestra el producto, el precio, la cantidad y el importe
     fields = ("producto", "cantidad", "importe")
 
@@ -31,11 +27,14 @@ class DetalleVentaInline(admin.TabularInline):
 
     extra = 1
 
+    form = DetalleVentaForm
+
+
 class VentaAdmin(admin.ModelAdmin):
 
     inlines = [DetalleVentaInline]
+
     list_display = ("fecha", "cliente", "total")
-    
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
@@ -74,52 +73,14 @@ class VentaAdmin(admin.ModelAdmin):
 
         return fieldsets
     
-    def save_formset(self, request, form, formset, change):
-        try:
-            with transaction.atomic():
-                total = 0
-                instances = formset.save(commit=False)
-                for instance in instances:
-                    if instance.producto.inventario.cantidad_stock < instance.cantidad:
-                        from django.contrib import messages
-                        messages.error(request, f"No hay suficiente stock para el producto {instance.producto}")
-                        raise ValidationError(f"No hay suficiente stock para el producto {instance.producto}")
-                    else:
-                        instance.importe = instance.cantidad * instance.producto.precio
-                        instance.producto.inventario.cantidad_stock -= instance.cantidad
-                        total += instance.importe
-                        instance.save()  # Mueve el guardado aquí
-                form.instance.total = total
-                form.instance.save()
-                formset.save_m2m()
-        except ValidationError as e:
-            # Rollback manual de la transacción en caso de excepción
-            transaction.set_rollback(True)
-        return super().save_formset(request, form, formset, change)
-         
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        obj.empleado = request.user
+        # obj.decrementar_stock()
+        # # * obtener total de la venta
+        # total = sum([detalle.importe for detalle in obj.detalles.all()])
+        # obj.total = total
+        # obj.save()
         
-
-        
-
-    # * procesos extra de la venta y los detalles
-    # def save_model(self, request, obj, form, change):
-    #     if obj:
-    #         with transaction.atomic():
-    #             obj.empleado = request.user
-    #             #calcula los importes de los detalles de la venta
-    #             total = 0
-    #             for detalle in obj.detalles.all():
-    #                 detalle.importe = detalle.cantidad * detalle.producto.precio
-    #                 detalle.producto.inventario.cantidad_stock -= detalle.cantidad
-    #                 total += detalle.importe
-
-    #             for detalle in obj.detalles.all():
-    #                 detalle.producto.inventario.save()
-    #                 detalle.save()
-
-    #             obj.total = total
-
-    #             obj.save()
-
-
+    
 admin.site.register(Venta, VentaAdmin)
