8ee19da51a3f6753ca62121ed9979ee62a90ee76
HetairoiElite <jonathan90090@gmail.com>
2024-05-13 15:10:21 -0600

    routeo de urls de la app control de ventas

--- control_ventas/views.py
+++ control_ventas/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from .models import Venta
 from rest_framework import viewsets
 from .serializers import VentaSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
 class VentaViewApi(viewsets.ModelViewSet):
     queryset = Venta.objects.all()
     serializer_class = VentaSerializer
+    
+# * api para conseguir el total de ventas de hoy
+# * djangorestframework
+
+class TotalVentasHoyApi(APIView):
+    def get(self, request):
+        total = Venta.objects.total_ventas_hoy()
+        return Response({'total': total})
