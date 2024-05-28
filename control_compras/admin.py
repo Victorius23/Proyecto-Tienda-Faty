bb096864116b1a82747c350acc8d9ffe3e61f1d2
HetairoiElite <jonathan90090@gmail.com>
2024-05-27 23:14:08 -0600

    detalle compra inline

--- control_compras/admin.py
+++ control_compras/admin.py
@@ -4,6 +4,23 @@ from django.contrib import admin
 
 from .models import Compra, DetalleCompra
 
+class DetalleCompraInline(admin.TabularInline):
+    model = DetalleCompra
+    extra = 1
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('producto', 'cantidad', 'importe')
+        else:
+            return ('importe',)
+    
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [DetalleCompraInline]
+    list_display = ('fecha', 'total', 'empleado')
+    list_filter = ('fecha', 'empleado')
+    search_fields = ('fecha', 'empleado')
+    date_hierarchy = 'fecha'
 
-admin.site.register(Compra)
+
+admin.site.register(Compra, CompraAdmin)
 
