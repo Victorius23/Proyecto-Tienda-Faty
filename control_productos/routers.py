fdeb9ef6f2dc7c8138b439de6b8bcb201b278bc3
HetairoiElite <jonathan90090@gmail.com>
2024-05-27 23:14:34 -0600

    app name

--- control_productos/routers.py
+++ control_productos/routers.py
@@ -5,6 +5,8 @@ from .views import ProductoViewApi, CategoriaViewApi, ProductoCategoriaViewApi
 
 router = routers.SimpleRouter()
 
+app_name = "control_productos"
+
 router.register("productos", ProductoViewApi)
 router.register("categorias", CategoriaViewApi)
 router.register("productos_categoria", ProductoCategoriaViewApi, basename="productos_categoria")
