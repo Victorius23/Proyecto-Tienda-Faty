ff072a92f9998e6781ce4cef5bb14d224d49bd27
Victorius23 <L20240002@smartin.tecnm.mx>
2024-05-27 18:26:17 -0600

    s

--- registration/urls.py
+++ registration/urls.py
@@ -8,7 +8,11 @@ from .views import UsuarioRegistrado
 app_name = 'registration'
 
 urlpatterns = [
-    path('empleado-mas-ventas/', VentaPorEmpleado.as_view(), name='empleado_mas_ventas'),
-    path('usuarios-registrados/', UsuarioRegistrado.as_view(), name='usuarios_registrados'),
+    path('empleado-mas-ventas/', 
+         VentaPorEmpleado.as_view(), 
+         name='empleado_mas_ventas'),
+    path('usuarios-registrados/', 
+         UsuarioRegistrado.as_view(), 
+         name='usuarios_registrados'),
 
 ]
